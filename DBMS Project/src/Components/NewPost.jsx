07c7a8c90938becc532e8c84d11ab6a7a65a81c1
Namitsai19082005 <namitsaivaddempudi19082005@gmail.com>
2024-10-07 17:46:29 +0530

    New Post page added

--- DBMS Project/src/Components/NewPost.jsx
+++ DBMS Project/src/Components/NewPost.jsx
@@ -4,6 +4,9 @@ import Coremembersidebar from "./Coremembersidebar.jsx"
 import Scroll from "./Scroll.jsx"
 import SubClubHeader from "./SubClubHeader.jsx"
 import Coremembermanage from "./Coremembermanage.jsx"
+import upload from "../assets/upload.png"
+import people1 from "../assets/people1.png"
+import arrow from "../assets/arrow.png"
 
 function NewPost()
 {
@@ -19,6 +22,21 @@ function NewPost()
                    <div className={styles.content}>
                        <div>
                            <Scroll></Scroll>
+                           <div className={styles.uploadimage}>
+                                <img className={styles.image} width="200px" src={upload}/>
+                                <p className={styles.text}>Upload</p>
+                           </div>
+                           <div>
+                               <p className={styles.text1}>Description</p>
+                               <hr/>
+                               <textarea className={styles.text2}>type here</textarea>
+                           </div>
+                           <div className={styles.tagpeople}>
+                               <img width="30px" height="30px" src={people1}/>
+                               <p className={styles.text3}>Tag people</p>
+                               <img width="15px" height="20px" className={styles.image1} src={arrow}/>
+                           </div>
+                           <button type="button" className={`${styles.btn} ${styles.btn1} ${styles.contact}`}>Post</button>
                         </div>
                        <div>
                            <Coremembermanage></Coremembermanage>
